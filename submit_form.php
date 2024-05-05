683f51bd26ff88a224d244a9bac179a9cba04e9e
zhay47 <55160279+zhay47@users.noreply.github.com>
2024-05-05 17:47:47 -0400

    Update submit_form.php

--- submit_form.php
+++ submit_form.php
@@ -1,20 +1,26 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
     $title = $_POST["title"];
-    $body = $_POST["body"];
+    $message = $_POST["body"];
 
-    // Email address to send the form submission
+    // Set up email parameters
     $to = "help@voltb.org";
-    $subject = "New Form Submission - Volt";
-    $message = "Title: $title\n\nMessage: $body";
+    $subject = "New Contact Form Submission - Volt";
+    $headers = "From: contact@voltb.org";
+    
+    // Construct email body
+    $email_body = "Title: $title\n\n";
+    $email_body .= "Message:\n$message";
 
     // Send email
-    if (mail($to, $subject, $message)) {
+    if (mail($to, $subject, $email_body, $headers)) {
         echo "<p>Your message has been sent successfully. We'll get back to you shortly.</p>";
     } else {
         echo "<p>Sorry, there was an error sending your message. Please try again later.</p>";
     }
 } else {
+    // If the request method is not POST, deny access
     echo "<p>Access denied.</p>";
 }
 ?>
